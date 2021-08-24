87bd4e877330d16343d37bfb2c5c39e9d660411c
Coditas <Coditas@DESKTOP-GEVL235>
2021-08-24 11:00:35 +0530

    AOP - Pointcut Expressions - Match only AcountDAO addAccount method, Match any add* method

--- spring-demo-aop/src/com/luv2code/aopdemo/MainDemoApp.java
+++ spring-demo-aop/src/com/luv2code/aopdemo/MainDemoApp.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 import com.luv2code.aopdemo.dao.AccountDAO;
 import com.luv2code.aopdemo.dao.MembershipDAO;
-
+ 
 public class MainDemoApp {
 
 	public static void main(String[] args) {
@@ -23,7 +23,7 @@ public class MainDemoApp {
 		theAccountDAO.addAccount();
 		
 		// call the membership business method
-		theMembershipDAO.addAccount();
+		theMembershipDAO.addSillyMember();
 		
 		// close the context
 		context.close();
--- spring-demo-aop/src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ spring-demo-aop/src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -12,13 +12,29 @@ public class MyDemoLoggingAspect {
 	
 	///// @Before ADVICE /////
 	
-	@Before("execution(public void addAccount())")
+	/*@Before("execution(public void addAccount())")*/
 	// "execution(public void addAccount())" is an Pointcut Expression AND it will
 	// call this method(beforeAddAccountAdvice()) for any execution of a method that
 	// has a signature 'public void addAccount()'.
 	// So this is @before Advice along with a Pointcut Expression.
 	// This will execute before addAccount() method in ANY CLASS.
 	// So this will also call addAccount() method from MembershipDAO class.
+	//
+	// MATCH ONLY AcountDAO addAccount() METHOD - 
+	// We can give fully qualified name of a class and method to only apply our 
+	// @Before advice method to it.
+	// So here, we give fully qualified name of our AccountDAO class so that this Advice 
+	// will now ONLY APPLY TO IT :
+	/*@Before("execution(public void com.luv2code.aopdemo.dao.AccountDAO.addAccount())")*/
+	//
+	// MATCH METHOD STARTING WITH "add" IN ANY CLASS - 
+	// We can use wild card pattern, we can specify pattern of method name to which our Aspect
+	// should be applied.
+	// We can give 'add*', so that it will apply to any method that starts with 'add'.
+	// So whole method name does not matter. It should only start with 'add'.
+	// So now, this will apply to our both methods addAccount() from AccountDAO AND
+	// addSillyMember() from MembershipDAO.
+	@Before("execution(public void add*())")
 	public void beforeAddAccountAdvice() { // We can give any method name.
 		
 		// Here we can add our own custom code.
